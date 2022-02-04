5d123a7e691c4877bf6f24f65457bb11c2273431
TahielGiordana <tahielgiordana@gmail.com>
2022-02-03 21:22:22 -0300

    Se cuentan las tareas restantes

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,15 +1,17 @@
 const todoList = document.querySelector(".content__list");
 const addTodoElement = document.getElementById("addNewTodo");
+const itemsLeft = document.getElementById("itemsLeft");
 
 //Create a new todo
 const addTodo = () => {
   let text = addTodoElement.value;
   todoList.appendChild(createCard(text));
+  itemsLeft.innerHTML = parseInt(itemsLeft.textContent) + 1;
 };
 
 const createCard = (text) => {
   let card = document.createElement("div");
-  card.classList.add("list-item");
+  card.classList.add("list-item", "active");
   let checkBtn = document.createElement("button");
   checkBtn.classList.add("list-item__check");
   checkBtn.addEventListener("click", markTodo);
@@ -35,9 +37,20 @@ addTodoElement.addEventListener("keyup", (event) => {
 const deleteTodo = (e) => {
   let card = e.target.parentElement;
   todoList.removeChild(card);
+  if (card.classList.contains("active")) {
+    itemsLeft.innerHTML = parseInt(itemsLeft.textContent) - 1;
+  }
 };
 
 //Mark a todo
 const markTodo = (e) => {
-  e.target.classList.toggle("checked");
+  if (e.target.classList.toggle("checked")) {
+    e.target.parentElement.classList.remove("active");
+    e.target.parentElement.classList.add("completed");
+    itemsLeft.innerHTML = parseInt(itemsLeft.textContent) - 1;
+  } else {
+    e.target.parentElement.classList.remove("completed");
+    e.target.parentElement.classList.add("active");
+    itemsLeft.innerHTML = parseInt(itemsLeft.textContent) + 1;
+  }
 };
